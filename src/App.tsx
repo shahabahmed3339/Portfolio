0fac0abf8ddf3eb9e445a13bc293db31fa8e9dfb
Shahab Ahmed <shahabahmed3339@gmail.com>
2026-02-18 19:42:14 +0500

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import html2canvas from 'html2canvas-pro';
+import jsPDF from "jspdf";
 import Footer from "./components/footer";
 import LenisScroll from "./components/lenis";
 import AboutSection from "./sections/about-section";
@@ -12,20 +14,66 @@ import ScrollToTop from './components/scroll-to-top';
 
 export default function App() {
     const contactRef = useRef<HTMLDivElement | null>(null);
+    const printRef = useRef<HTMLDivElement | null>(null);
+    const [isDownloading, setIsDownloading] = useState(false);
+
+    const handleDownloadPdf = async () => {
+        if (!printRef.current) {
+            console.error("Nothing to print — printRef is null!");
+            return;
+        }
+
+        setIsDownloading(true);
+        setTimeout(async () => {
+            try {
+                const element = printRef.current;
+                const canvas = await html2canvas(element!, { scale: 2 });
+                const imgData = canvas.toDataURL("image/png");
+
+                const pdf = new jsPDF("p", "mm", "a4");
+                const imgProps = pdf.getImageProperties(imgData);
+                const pdfWidth = pdf.internal.pageSize.getWidth();
+                const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+
+                let heightLeft = pdfHeight;
+                let position = 0;
+
+                pdf.addImage(imgData, "PNG", 0, position, pdfWidth, pdfHeight);
+                heightLeft -= pdf.internal.pageSize.getHeight();
+
+                while (heightLeft > 0) {
+                    position = heightLeft - pdfHeight;
+                    pdf.addPage();
+                    pdf.addImage(imgData, "PNG", 0, position, pdfWidth, pdfHeight);
+                    heightLeft -= pdf.internal.pageSize.getHeight();
+                }
+
+                pdf.save("portfolio.pdf");
+            } catch (err) {
+                console.error(err);
+            }
+            setIsDownloading(false);
+        }, 0);
+    };
+
     return (
-        <main className="max-md:px-4">
+        <main className="max-md:px-4 lg:px-100">
             <LenisScroll />
             <ScrollToTop />
-            <HeroSection
-                onContactClick={() => contactRef.current?.scrollIntoView({ behavior: "smooth" })}
-            />
-            <AboutSection />
-            <ExperienceSection />
-            <EducationSection />
-            {/* <ProjectsSection /> */}
-            <SkillsSection />
-            <div ref={contactRef}>
-                <ContactSection />
+            <div ref={printRef}>
+                <HeroSection
+                    onContactClick={() => contactRef.current?.scrollIntoView({ behavior: "smooth" })}
+                    onDownloadClick={handleDownloadPdf}
+                    isDownloading={isDownloading}
+                />
+                <AboutSection />
+                <ExperienceSection />
+                <EducationSection />
+                {/* <ProjectsSection /> */}
+                <SkillsSection />
+                <div ref={contactRef}>
+                    <ContactSection />
+                </div>
             </div>
             <Footer />
         </main>
--- src/sections/hero-section.tsx
+++ src/sections/hero-section.tsx
@@ -2,9 +2,11 @@ import { ArrowRightIcon } from "lucide-react";
 
 type HeroSectionProps = {
     onContactClick: () => void;
+    onDownloadClick: () => void;
+    isDownloading: boolean;
 }
 
-export default function HeroSection({ onContactClick }: HeroSectionProps) {
+export default function HeroSection({ onContactClick, onDownloadClick, isDownloading }: HeroSectionProps) {
     return (
         <main className="flex flex-col items-center pt-32 max-md:px-4">
             <svg className="absolute -z-10 -mt-20 right-0" width="890" height="764" viewBox="0 0 890 764" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -50,17 +52,19 @@ export default function HeroSection({ onContactClick }: HeroSectionProps) {
             <p className="text-2xl md:text-3xl font-mono mt-6">
                 Software Engineer
             </p>
-            <div className="mt-6">
-                <a href="/assets/Shahab-Ahmed-Resume.pdf" download="Shahab-Ahmed-Resume.pdf">
-                    <button className="group bg-indigo-600 hover:bg-indigo-700 text-white px-7 py-2.5 rounded-lg">
+            {!isDownloading && (
+                <div className="mt-6">
+                    {/* <a href="/assets/Shahab-Ahmed-Resume.pdf" download="Shahab-Ahmed-Resume.pdf"> */}
+                    <button className="group bg-indigo-600 hover:bg-indigo-700 text-white px-7 py-2.5 rounded-lg" onClick={onDownloadClick}>
                         Resume
                         <ArrowRightIcon className="group-hover:translate-x-0.5 transition ml-1 size-5 inline-flex" />
                     </button>
-                </a>
-                <button className="border border-gray-400 hover:bg-gray-100/70 px-6 py-2.5 rounded-lg ml-4" onClick={onContactClick}>
-                    Connect me
-                </button>
-            </div>
+                    {/* </a> */}
+                    <button className="border border-gray-400 hover:bg-gray-100/70 px-6 py-2.5 rounded-lg ml-4" onClick={onContactClick}>
+                        Connect me
+                    </button>
+                </div>
+            )}
             <div className="space-x-6 mt-10">
                 <a href="https://github.com/shahabahmed3339" target="_blank" className="inline-flex hover:-translate-y-0.5 transition">
                     <svg height={24} width={24} role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path fill="#181717" d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" /></svg>
